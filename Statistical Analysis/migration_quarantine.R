a4897c3879c7363a41d4b9239b6006cf0b5c8b64
troisoong <114517716+troisoong@users.noreply.github.com>
2023-05-01 18:55:43 +0100

    Update migration_quarantine.R

--- Statistical Analysis/migration_quarantine.R
+++ Statistical Analysis/migration_quarantine.R
@@ -46,36 +46,4 @@ infection_migration             0.0228  1    0.87995
 teleporters:infection_migration 4.2684  1    0.03883 *"
 #significant interaction detected
 
-#plot graph to visualise results
-plot_migration <- ggplot(migrationdata, aes(x=teleporters, y=peak_prop,
-                                            color=infection_migration)) + 
-  geom_point(size=0.7) + 
-  labs(x="Teleporters per Village", y="Proportion of Population Infected") +
-  scale_color_manual(labels = c("Infectious Villagers Quarantined",
-                                "Infectious Villagers Free to Migrate"),values = c("#D95F02","#7570B3")) +
-  geom_smooth(method = "lm", formula= (y ~ x), aes(color = infection_migration), 
-              size=1.5,se = TRUE) + 
-  scale_y_continuous(expand = c(0,0), limits = c(0,0.3), breaks=seq(0,0.3, by=0.05)) +
-  theme(panel.border = element_rect(colour = "black", fill=NA),
-        panel.background = element_rect(fill="white"), 
-        legend.key = element_blank(),
-        axis.text = element_text(colour="black"),
-        axis.title.x = element_text(size = 20,vjust = -0.4),
-        axis.title.y = element_text(size = 20,vjust = 2),
-        axis.text.x = element_text(size = 15),
-        axis.text.y = element_text(size = 15),
-        legend.title = element_text(size = 17.5),
-        legend.text = element_text(size = 15),
-        aspect.ratio=2/1) +
-  guides(color=guide_legend(" "))
-
-#display graph
-plot_migration
-
-#save to computer
-ggsave("plot_migration_summary.png",dpi=300)
-
-
-
-
-
+#graphs plotted with ggplot
