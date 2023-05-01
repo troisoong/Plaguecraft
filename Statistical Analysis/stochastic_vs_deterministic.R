d7bdbf638f2631be61d4c083b132affb8eb09b28
troisoong <114517716+troisoong@users.noreply.github.com>
2023-05-01 18:59:28 +0100

    Update stochastic_vs_deterministic.R

--- Statistical Analysis/stochastic_vs_deterministic.R
+++ Statistical Analysis/stochastic_vs_deterministic.R
@@ -32,27 +32,6 @@ Response: max_R
 model       1 453.00  453.00  227.22 < 2.2e-16 ***
 Residuals 198 394.75    1.99 "
 #suggests significant difference between groups
+#residuals all look fine
 
-#plot to illustrate results
-plot_stodet<- ggplot(stodetdata, aes(x=model, y=max_R, fill=model)) + 
-  geom_boxplot() + 
-  scale_fill_manual(name="Model type",labels=c("Deterministic", "Stochastic"),
-  values = c("#D95F02","#7570B3")) + 
-  labs(x=" ", y="Maximum R number") + 
-  scale_x_discrete(labels=c("Deterministic","Stochastic")) +
-  scale_y_continuous(expand = c(0,0), limits = c(0,14), breaks=seq(0,14, by=2)) +
-  theme(panel.border = element_rect(colour = "black", fill=NA),
-        panel.background = element_rect(fill="white"), 
-        legend.key = element_blank(),
-        axis.text = element_text(colour="black"),
-        axis.title.x = element_text(size = 28),
-        axis.title.y = element_text(size = 28,vjust = 2),
-        axis.text.x = element_text(size = 21),
-        axis.text.y = element_text(size = 23),
-        legend.title = element_text(size = 17.5),
-        legend.text = element_text(size = 15),
-        plot.margin = margin(t=20,l=20))
-plot_stodet
-
-#save plot to computer
-ggsave("plot_stodet.png",dpi=300)
+#graphs plotted with ggplot
